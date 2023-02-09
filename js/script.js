f8bd947f8d809e47cfdb2fa2aa24ad3bdc455d49
Estudiante <estuam@UAM.LOCAL>
2023-02-09 11:58:25 -0500

    feat: build template and index menu

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("Hello world");
+var a=4;
+var b=16;
+var c=a+b
+console.log(c);
+
+
+var x=5;
+var y=20;
+var z=x*y;
+console.log("El resultado de la multiplicacion es: "+z);
+
+
+var raiz=Math.sqrt(1244);
+console.log("La raiz cuadrada de 1244 es: "+raiz);
+
+var raiz2=Math.trunc(raiz);
+console.log("Esto, aproximado al numero entero mas cercano, es igual a: "+raiz2);
